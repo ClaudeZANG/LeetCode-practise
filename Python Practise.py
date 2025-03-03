f04616974a52c91c93715b3ee4321b91e6c99b65
ClaudeZANG <chuanjiezang@gmail.com>
2025-03-03 14:59:29 -0800

    Auto-update LeetCode practise folder

--- Python Practise.py
+++ Python Practise.py
@@ -638,6 +638,51 @@
 # 1 <= prices.length <= 3 * 104
 # 0 <= prices[i] <= 104
 
+# class Solution:
+#     def maxProfit(self, prices):
+#         return sum(max(prices[i] - prices[i - 1], 0) for i in range (1, len(prices)))
 
+# re-write 2025/Mar/1 -1
+# class Solution:
+#     def maxProfit(self, prices):
+#         return sum(max(prices[i] - prices[i - 1], 0) for i in range (1, len(prices)))
 
-# re-write 2025/Feb/28 -1
+# re-write 2025/Mar/1 -2
+# class Solution:
+#     def maxProfit(self, prices):
+#         return sum(max(prices[i] - prices[i - 1], 0) for i in range (1, len(prices)))
+
+# -----------------------------------
+
+# 55. Jump Game
+# Medium # Topics # Companies
+# You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+# Return true if you can reach the last index, or false otherwise.
+
+# Example 1:
+# Input: nums = [2,3,1,1,4]
+# Output: true
+# Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+# Example 2:
+# Input: nums = [3,2,1,0,4]
+# Output: false
+# Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+
+# Constraints:
+# 1 <= nums.length <= 104
+# 0 <= nums[i] <= 105
+
+class Solution:
+    def canJump(self, nums):
+        max_reach = 0
+        for i in range(len(nums)):
+            if i > max_reach:
+                return False
+            max_reach = max(max_reach, i + nums[i])
+            if max_reach >= len(nums) - 1:
+                return True
+        return True
+
+# re-write 2025/Mar/1 - 1
+
